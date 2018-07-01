9967907058e862fbbcf18867b89cb25d237d534e
MarsLinHao <a_mars0413@hotmail.com>
2018-07-01 21:23:32 +0800

    Completed section 2 tutorial

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,7 +11,9 @@ router
 
 router
     .route('/hotels/:hotelId')
-    .get(ctrlHotels.hotelsGetOne);
+    .get(ctrlHotels.hotelsGetOne)
+    .put(ctrlHotels.hotelsUpdateOne)
+    .delete(ctrlHotels.hotelsDeleteOne);
 
 //Review route
 router
@@ -21,6 +23,8 @@ router
 
 router
     .route('/hotels/:hotelId/reviews/:reviewId')
-    .get(ctrlReviews.reviewsGetOne);
+    .get(ctrlReviews.reviewsGetOne)
+    .put(ctrlReviews.reviewsUpdateOne)
+    .delete(ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;
